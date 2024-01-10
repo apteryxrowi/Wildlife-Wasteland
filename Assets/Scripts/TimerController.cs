e45df1706777731d3b72fd57e5390f587e12ff92
apteryx_rowi <kevindakiwi6000@gmail.com>
2024-01-10 12:19:44 -0500

    Timer added!

---
+++ Assets/Scripts/TimerController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerController : MonoBehaviour
+{
+    public float minutes = 20;
+    private protected float Timer;
+    private Text myText;
+    // Update is called once per frame
+    private void Start()
+    {
+        Timer = minutes * 60;
+        myText = GetComponent<Text>();
+        myText.text = minutes + " : " + "00";
+    }
+    void Update()
+    {
+        Timer -= Time.deltaTime;
+        float x = Mathf.FloorToInt(Timer / 60);
+        float y = Mathf.FloorToInt(((Timer / 60) % 1) * 60);
+        if (y < 10)
+        {
+            myText.text = x + " : 0" + y;
+        }
+        else
+        {
+            myText.text = x + " : " + y;
+        }
+    }
+}
