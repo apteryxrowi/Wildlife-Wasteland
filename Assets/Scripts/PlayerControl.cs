1bea724dff92fb01d0f6f320a9852b01956670b0
apteryx_rowi <kevindakiwi6000@gmail.com>
2024-01-26 22:48:18 -0500

    You can actually drink the water now!

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -26,6 +26,7 @@ public class PlayerControl : MonoBehaviour
             transform.position = new Vector3(0, 3.6f, 0);
             HealthHUDController.health = 50;
             EnergyHUDController.Energy = 60;
+            HydrationHUD.Hydration = 60;
         }
     }
     void Move()
